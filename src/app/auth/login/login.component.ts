852b8e403cae0354137e38dd58a2105a59660a12
andres felipe <andf2019@gmail.com>
2023-11-15 12:46:46 -0500

    AVANCE

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  formularioLogin: FormGroup = this.fb.group({
+    usuario: ['', Validators.required],
+    clave: ['', Validators.required],
+    tipo: ['', Validators.required],
+  });
 
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  login() {
+    const loginInfo: LoginInfo = {
+      usuario: this.loginForm.get('usuario')?.value,
+      clave: this.loginForm.get('clave')?.value,
+      id_rol: this.loginForm.get('rol')?.value,
+    };
+
+    this.authService.login(loginInfo).subscribe((res) => {
+      console.log(res);
+
+      if (res.login) {
+        console.log('LOGIN CORRECTO');
+        console.log(res);
+
+        this.authService.userAuthInfo = {
+          rol_id: loginInfo.id_rol,
+          rol: rolNombre,
+        };
+
+        // * Se crea la sesion
+        this.authService
+          .crearSesionUsuario({
+            id_usuario: this.authService.userIdRequest,
+          })
+          .subscribe((res) => {
+            switch (this.authService.userAuthInfo.rol.toLowerCase()) {
+              case 'estudiante':
+                this.router.navigate(['/student']);
+                break;
+              case 'docente':
+                this.router.navigate(['/docente']);
+                break;
+              case 'administrativo':
+                this.router.navigate(['/admin']);
+                break;
+
+              default:
+                // TODO: alerta de error de login. ROl no encontrado
+                break;
+            }
+          });
+      }
+    });
+  }
 }
